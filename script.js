b62cab941618f1edd10040dd0894296c979db600
Asmau12345 <asmauomar2@gmail.com>
2026-07-04 08:09:17 -0700

    Implement smooth scrolling and fade-in effect
    Added fade-in animation on scroll for sections.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 document.querySelectorAll('a[href^="#"]').forEach(link=>{
     link.addEventListener("click",function(e){
+        
         e.preventDefault();
-
         const target = document.querySelector(this.getAttribute("href"));
-        target.scrollIntoView({
-            behavior: "smooth"
-        });
+        target.scrollIntoView({ behavior: "smooth" )}
             });
         });
+//Fade-in animation
+const sections= document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(sec => {
+        const top = 
+            sec.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            sec.style.opacity = 1;
+            sec.style.transform = "translateY(0)";
+        }
+    });
+});
