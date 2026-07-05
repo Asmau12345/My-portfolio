49fed5b4e7cb7fe06730e6220f613da981085b6d
Asmau12345 <asmauomar2@gmail.com>
2026-07-05 01:57:52 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-document.querySelectorAll('a[href^="#"]').forEach(link=>{
-    link.addEventListener("click",function(e){
-        e.preventDefault();
+const cube =
+    document.getElementById("cube");
+document.addEventListener("mousemove", (e) =>  {
+    let x  = (e. clientX/ window.innerwidth- 0.5)* 360;
+    let y = (e.clientY/ window.innerHeight - 0.5) *360;
 
-        const target = document.querySelector(this.getAttribute("href"));
-        target.scrollIntoView({
-            behavior: "smooth"
-        });
-            });
-        });
-const sections = document.querySelectorAll("section");
+    cube.style.transform = 'rotateX(${-y} deg) rotateY(${x}deg)';
+});
 
-window.addEventListener("scroll", () => {
-    sections.forEach(sec => {
-        const top =
-            sec.getBoundingClientRect(). top;
-        if (top < window.innerHeight - 100){
-            sec.style.opacity = 1;
-            sec.style.trasform = "translateY(0)";
-        }
-        const materials = [
-            new THREE.MeshBasicMaterial({ color: 0x00ffcc }),
-            new THREE.MeshBasicMaterial({ color: 0xff00ff }),
-            new THREE.MeshBasicMaterial({ color: 0x0088ff }),
-            new THREE.MeshBasicMaterial({ color: 0xffff00 }),
-            new THREE.MeshBasicMaterial({ color: 0xff5500 }),
-            new THREE.MeshBasicMaterial({ color: 0xffffff })
-        ];
-    })
-})
+
