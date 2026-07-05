d7ec4dbf4855f5668f5f1e50dcaab2a01016390b
Asmau12345 <asmauomar2@gmail.com>
2026-07-05 05:21:54 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ document.querySelector("button").addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+const elements = document.querySelectorAll("section");
 
+window.addEventListener("scroll", () => {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    elements.forEach(el => {
+        const boxTop = el.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+            el.style.opacity = "1";
+            el.style.transform = "translateY(0)";
+        }
+    });
+});
